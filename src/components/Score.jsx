846770685d9dc4cb1f78691ae5570778d4d68518
Ali Kirat <91100475+peripateticlearner@users.noreply.github.com>
2025-03-02 17:32:31 -0800

    added css

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,7 +1,7 @@
 function Score({ date, score }) {
     return (
       <li>
-        <strong>Date:</strong> {date} - <strong>Score:</strong> {score}
+        <strong>Date:</strong> {date} --- <strong>Score:</strong> {score}
       </li>
     );
   }
